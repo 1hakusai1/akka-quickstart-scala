e896aafbe84d3062e0d0bcf70b22e3a7f62ba815
1hakusai1 <1hakusai1@gmail.com>
2022-10-04 17:52:19 +0900

    終了シグナルに反応する

--- src/main/scala/com/example/ActorHierarchyExperiments.scala
+++ src/main/scala/com/example/ActorHierarchyExperiments.scala
@@ -1,11 +1,12 @@
 package com.example
 
 import akka.actor.ActorSystem
+import akka.actor.TypedActor
+import akka.actor.typed
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.AbstractBehavior
 import akka.actor.typed.scaladsl.ActorContext
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed
 
 object PrintMyActorRefActor {
     def apply(): Behavior[String] =
@@ -37,12 +38,50 @@ class Main(context: ActorContext[String])
     override def onMessage(msg: String): Behavior[String] =
         msg match {
             case "start" =>
-                val firstRef =
-                    context.spawn(PrintMyActorRefActor(), "first-actor")
-                println(s"First: $firstRef")
-                firstRef ! "print"
+                val first = context.spawn(StartStopActor1(), "first")
+                first ! "stop"
                 this
         }
+}
+object StartStopActor1 {
+    def apply(): Behavior[String] =
+        Behaviors.setup(context => new StartStopActor1(context))
+}
+
+class StartStopActor1(context: ActorContext[String])
+    extends AbstractBehavior[String](context) {
+    println("first start")
+    context.spawn(StartStopActor2(), "second")
+
+    override def onMessage(msg: String): Behavior[String] =
+        msg match {
+            case "stop" => Behaviors.stopped
+        }
+
+    override def onSignal: PartialFunction[typed.Signal, Behavior[String]] = {
+        case typed.PostStop =>
+            println("first stopped")
+            this
+    }
+}
+
+object StartStopActor2 {
+    def apply(): Behavior[String] = Behaviors.setup(new StartStopActor2(_))
+}
+
+class StartStopActor2(context: ActorContext[String])
+    extends AbstractBehavior[String](context) {
+    println("second start")
+    override def onMessage(msg: String): Behavior[String] = {
+        Behaviors.unhandled
+    }
+
+    override def onSignal: PartialFunction[typed.Signal, Behavior[String]] = {
+        case typed.PostStop =>
+            println("second stopped")
+            this
+    }
+
 }
 
 object ActorHierarchyExperiments extends App {
