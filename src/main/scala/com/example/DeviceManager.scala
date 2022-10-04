2bc735a336aacc0439a245778d8dc01eb68c35ea
1hakusai1 <1hakusai1@gmail.com>
2022-10-04 22:37:01 +0900

    DeviceManagerの実装
    テストは読者への宿題みたいな感じになっているが、DeviceGroupのテストでほぼカバーされている気がする

--- src/main/scala/com/example/DeviceManager.scala
+++ src/main/scala/com/example/DeviceManager.scala
@@ -1,6 +1,11 @@
 package com.example
 
 import akka.actor.typed.ActorRef
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.Behavior
+import akka.actor.typed.Signal
+import akka.actor.typed.PostStop
 
 object DeviceManager {
     sealed trait Command
@@ -19,4 +24,61 @@ object DeviceManager {
     ) extends Command
         with DeviceGroup.Command
     final case class ReplyDeviceList(requestId: Long, ids: Set[String])
+    private final case class DeviceGroupTerminated(groupId: String)
+        extends Command
+}
+
+class DeviceManager(context: ActorContext[DeviceManager.Command])
+    extends AbstractBehavior[DeviceManager.Command](context) {
+    import DeviceManager._
+
+    var groupIdToActor = Map.empty[String, ActorRef[DeviceGroup.Command]]
+    context.log.info("DeviceManager started")
+
+    override def onMessage(
+        msg: DeviceManager.Command
+    ): Behavior[DeviceManager.Command] =
+        msg match {
+            case tracMsg @ RequestTrackDevice(groupId, _, replyTo) =>
+                groupIdToActor.get(groupId) match {
+                    case Some(ref) =>
+                        ref ! tracMsg
+                    case None =>
+                        context.log.info(
+                          "Creating device group actor for {}",
+                          groupId
+                        )
+                        val groupActor = context.spawn(
+                          DeviceGroup(groupId),
+                          s"group-$groupId"
+                        )
+                        context.watchWith(
+                          groupActor,
+                          DeviceGroupTerminated(groupId)
+                        )
+                        groupActor ! tracMsg
+                        groupIdToActor += groupId -> groupActor
+                }
+                this
+            case req @ RequestDeviceList(requestId, groupId, replyTo) =>
+                groupIdToActor.get(groupId) match {
+                    case Some(ref) =>
+                        ref ! req
+                    case None =>
+                        replyTo ! ReplyDeviceList(requestId, Set.empty)
+                }
+                this
+            case DeviceGroupTerminated(groupId) =>
+                context.log.info(
+                  "Device group actor for {} has been terminated",
+                  groupId
+                )
+                groupIdToActor -= groupId
+                this
+        }
+    override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
+        case PostStop =>
+            context.log.info("DeviceManager stopped")
+            this
+    }
 }
